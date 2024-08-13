469734aea186ae72eddac52b704483ad37737e26
lara.encoding <analara.cafe@gmail.com>
2024-08-13 18:33:48 +0100

    implementando o estudo Utilizando e Testando o Bean Validation.

--- src/main/java/io/github/dougllasfps/rest/controller/ApplicationControllerAdvice.java
+++ src/main/java/io/github/dougllasfps/rest/controller/ApplicationControllerAdvice.java
@@ -4,10 +4,14 @@ import io.github.dougllasfps.exception.PedidoNaoEncontradoException;
 import io.github.dougllasfps.exception.RegraNegocioException;
 import io.github.dougllasfps.rest.ApiErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -23,5 +27,15 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiErrors handlePedidoNotFoundException( PedidoNaoEncontradoException ex ){
         return new ApiErrors(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleMethodNotValidException( MethodArgumentNotValidException ex ){
+        List<String> errors = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map( erro -> erro.getDefaultMessage() )
+                .collect(Collectors.toList());
+        return new ApiErrors(errors.toString());
     }
 }
