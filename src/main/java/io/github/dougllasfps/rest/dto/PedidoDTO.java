e3963e7f8e26a071ec33fe1dbefec799ffe4106a
lara.encoding <analara.cafe@gmail.com>
2024-08-15 17:25:09 +0100

    implementando o estudo Criando uma annotation de validação customizada.

--- src/main/java/io/github/dougllasfps/rest/dto/PedidoDTO.java
+++ src/main/java/io/github/dougllasfps/rest/dto/PedidoDTO.java
@@ -1,5 +1,6 @@
 package io.github.dougllasfps.rest.dto;
 
+import io.github.dougllasfps.validation.NotEmptyList;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,10 @@ import java.util.List;
 public class PedidoDTO {
     @NotNull(message = "Informe o código do cliente.")
     private Integer cliente;
+
     @NotNull(message = "Campo Total do pedido é obrigatório.")
     private BigDecimal total;
+
+    @NotEmptyList(message = "Pedido não pode ser realizado sem itens.")
     private List<ItemPedidoDTO> items;
 }
