077f4f7ff21b4f57bbb5dee04c80cfea965c58e9
lara.encoding <analara.cafe@gmail.com>
2024-08-28 10:31:11 +0100

    implementando o estudo Gerando um Arquivo WAR.

--- src/main/java/io/github/dougllasfps/VendasApplication.java
+++ src/main/java/io/github/dougllasfps/VendasApplication.java
@@ -1,23 +1,11 @@
 package io.github.dougllasfps;
 
-import io.github.dougllasfps.domain.entity.Cliente;
-import io.github.dougllasfps.domain.repository.Clientes;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VendasApplication {
-
-    /*@Bean
-    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
-        return args -> {
-            Cliente c = new Cliente(null, "Fulano");
-            clientes.save(c);
-    };
-}*/
+public class VendasApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
